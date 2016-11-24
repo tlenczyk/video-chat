5e3dd94dfd3a9eec3ddb8e74bf3b854dffb78c06
tomek <tomasz.lenczyk@gmail.com>
2016-11-25 00:17:11 +0100

    adding fetchClient

---
+++ src/main/frontend/src/commons/fetch-client.js
@@ -0,0 +1,27 @@
+import {inject} from 'aurelia-framework';
+import {AureliaFetchClient} from 'aurelia-fetch-client';
+
+@inject(AureliaFetchClient)
+export class FetchClient {
+
+  constructor(httpClient){
+    this.httpClient = httpClient;
+  }
+
+  get(url) {
+    return this.httpClient.fetch(url)
+      .then((r) => r.json());
+  }
+
+  post(url, body) {
+    return this.httpClient.fetch(url, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    })
+      .then((r) => r.json());
+  }
+}
